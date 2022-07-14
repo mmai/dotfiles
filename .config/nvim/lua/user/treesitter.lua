c0e7c2d71ed0f124a205438092d02fcd57254c53
Henri Bourcereau <henri.bourcereau@gmail.com>
2022-07-14 11:52:22 +0200

    nvim lua

---
+++ .config/nvim/lua/user/treesitter.lua
@@ -0,0 +1,17 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+	ensure_installed = "all", -- one of "all" or a list of languages
+	ignore_install = { "ocamllex", "teal", "swift", "phpdoc", "d", "devicetree", "godot_resource" }, -- List of parsers to ignore installing (because their install fails)
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "css" }, -- list of language that will be disabled
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+})
