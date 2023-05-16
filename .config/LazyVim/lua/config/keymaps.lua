086d74ebcb8f21bf8eef176850830aa1ff79ca3c
Henri Bourcereau <henri.bourcereau@gmail.com>
2023-05-16 17:23:38 +0200

    LazyVim keymaps

--- .config/LazyVim/lua/config/keymaps.lua
+++ .config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "JK", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+keymap("i", "KJ", "<ESC>", opts)
+
+-- Close buffers with pmoll/vim-bbye
+keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
