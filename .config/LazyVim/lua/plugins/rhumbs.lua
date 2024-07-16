22a6b22c48492760d0ca6f36ecfa72c6d50f9a02
Henri Bourcereau <henri.bourcereau@gmail.com>
2024-07-16 10:48:55 +0200

    neovim : obsidian

--- .config/LazyVim/lua/plugins/rhumbs.lua
+++ .config/LazyVim/lua/plugins/rhumbs.lua
@@ -212,6 +212,39 @@ return {
       { ",<CR>", "<C-w>v:MdwiGotoLink<CR>", opts },
     },
   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/think",
+        },
+        {
+          name = "work",
+          path = "~/think/atixnet",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
   { "gpanders/editorconfig.nvim" }, -- follow formatting options of .editorconfig file
   {
     "tpope/vim-fugitive", -- for Git blame
